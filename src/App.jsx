48fae362837e423d03afee65831bba95b0913823
ck-sarvaiya <94895721+ck-sarvaiya@users.noreply.github.com>
2022-07-19 16:50:30 +0530

    Add the signup design-19/7/22

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import "./App.css";
 import { Switch } from "react-router-dom";
-
+import { SignUpForm } from "./components/SignUpForm"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            HOme
-          </Route>
-          <Route path="/about">About</Route>
-          <Route path="/dashboard">Dashboard</Route>
-        </Switch>
-      </Router>
+    <div className="app">
+      <SignUpForm />
     </div>
   );
 }
